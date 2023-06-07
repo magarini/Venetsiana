2ba80230e4d1b32fb03f4a959825ea4079876f06
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-06-07 13:41:33 +0300

    converted it to take line instead of word

--- scatter.js
+++ scatter.js
@@ -1,10 +1,34 @@
+// const enhance = () => {
+//     document.querySelectorAll(".word").forEach((word) => {
+//       const letters = word.innerText.split("");
+//       word.innerHTML = null;
+//       letters.forEach((letter) => {
+//         word.innerHTML += `<span class="letter">${letter}</span>`;
+//       });
+//     });
+//   };
+//   enhance();
+
+
+
+let str = document.querySelector('.textMsg')
+let words = str.innerHTML.split(' ').filter(w => w !== '');
+console.log(words)
+str.innerHTML = '';
+words.forEach((word) => {
+    str.innerHTML+= `<span class="word">${word}</span>`;
+})
+
+console.log(str.innerHTML);
+
+
 const enhance = () => {
     document.querySelectorAll(".word").forEach((word) => {
-      const letters = word.innerText.split("");
-      word.innerHTML = null;
-      letters.forEach((letter) => {
-        word.innerHTML += `<span class="letter">${letter}</span>`;
-      });
+        const letters = word.innerText.split("");
+        word.innerHTML = null;
+        letters.forEach((letter) => {
+            word.innerHTML += `<span class="letter">${letter}</span>`;
+        });
     });
-  };
-  enhance();
+};
+enhance();
