25d6512538583f1daa2126e9c365e39ffa687b61
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-06-15 15:04:54 +0300

    updated index.html

--- scatterAway.js
+++ scatterAway.js
@@ -23,29 +23,17 @@ enhance();
 
 
 document.querySelectorAll('.scatterIntro .letter').forEach((letter) => {
-    // let randomNumber=Math.random(-20,20)
-    let bodyRect=document.body.getBoundingClientRect();
+
     let letterPos = letter.getBoundingClientRect();
-    let offsetBottom=letterPos.bottom-bodyRect.bottom
-    let offsetLeft=letterPos.left-bodyRect.left;
-    console.log('initialPos', letterPos)
-    // letter.style.left = `${letterPos.left}px`;
-    // letter.style.bottom = `${letterPos.bottom}px`;
-    // letter.style.right = `${letterPos.right}px`;
 
-    letter.style.bottom=offsetBottom;
-    letter.style.left=offsetLeft;
-    // letter.style.position = "absolute";
-
-    letter.style.transition = "all 0.2s"
     letter.addEventListener('mouseover', () => {
         if (letterPos.x < window.innerWidth - 100 && letterPos.x > 0 && letterPos.y > 0 && letterPos.y < window.innerHeight - 100) {
-            letter.style.position = "absolute";
-            letter.style.left = `${offsetLeft + Math.floor(Math.random(-5, 5) * 100)}px`;
-            letter.style.bottom = `${offsetBottom/ 10 + Math.floor(Math.random(-5, 5) * 100)}px`;
-            letter.style.right = `${letterPos.right + Math.floor(Math.random(-5, 5) * 100)}px`;
+            letter.style.position = "relative";
+            letter.style.left = `${Math.floor(Math.random(-5, 5) * 100)}px`;
+            letter.style.bottom = `${Math.floor(Math.random(-5, 5) * 100)}px`;
+            letter.style.right = `${Math.floor(Math.random(-5, 5) * 100)}px`;
             // letter.style.top = `${letterPos.top / 10 + Math.floor(Math.random(-50, 50) * 100)}px`;
-            letter.style.transform=`rotate(${Math.floor(Math.random(-10, 10) * 10)}deg)`
+            letter.style.transform=`rotate(${Math.floor(Math.random(-90, 90))})`
             
             console.log('initialPosLeft', letterPos.left, letter.style.left)
 
