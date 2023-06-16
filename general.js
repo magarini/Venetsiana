feca812df4033797045024aff86da8fc2056780d
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-06-16 11:42:52 +0300

    added some icons

---
+++ general.js
@@ -0,0 +1,19 @@
+function toggleBurger() {
+    var menu = document.querySelector(".menu");
+    var navBar=document.querySelector(".burgerMenu");
+    var x=document.querySelector(".burger");
+
+    if (menu.style.display === "flex") {
+      menu.style.display = "none";
+      navBar.style.background="transparent"
+      x.innerHTML='<img src="icons/menu.svg">';
+      console.log('innerHtml',x.innerHTML)
+
+    } else {
+      menu.style.display = "flex";
+      navBar.style.background="#F2F2F2"
+      x.innerHTML='<img src="icons/close.svg">';
+
+
+    }
+  } 
--- scatterAway.js
+++ scatterAway.js
@@ -32,7 +32,10 @@ document.querySelectorAll('.scatterIntro .letter').forEach((letter) => {
             letter.style.left = `${Math.floor(Math.random(-5, 5) * 100)}px`;
             letter.style.bottom = `${Math.floor(Math.random(-5, 5) * 100)}px`;
             letter.style.right = `${Math.floor(Math.random(-5, 5) * 100)}px`;
-            letter.style.transform = `rotate(${Math.floor(Math.random(-90, 90))})`
+            letter.style.transform = `rotate(${Math.floor(Math.random(-90, 90)*100)}deg)`
+            letter.style.mozTransform= `rotate(${Math.floor(Math.random(-90, 90)*100)}deg)`
+            letter.style.webkitTransform= `rotate(${Math.floor(Math.random(-90, 90)*100)}deg)`
+
         }
     })
 })
