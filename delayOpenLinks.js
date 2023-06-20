30fa686c605553b019f7b7913ffd6c0f83459206
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-06-20 16:24:28 +0300

    delay 2 secs

--- delayOpenLinks.js
+++ delayOpenLinks.js
@@ -1,28 +1,11 @@
 
-
-// document.querySelectorAll('.project').forEach(project => {
-//     project.addEventListener('click', delayFunction);
-//     console.log('clickeddd')
-// })
-
-// function delayFunction(e) {
-//     e.preventDefault();
-//     let link = this.getAttribute("href");
-//     setTimeout(function () {
-//         window.location.href = link;
-//     }, 5000);
-// }
-
-
-
-
 document.querySelectorAll('.project').forEach((project)=> {
     project.addEventListener('click', (e)=>{
         e.preventDefault();
             let link = project.getAttribute("href");
             setTimeout(function () {
                 window.location.href = link;
-            }, 100);
+            }, 200);
             console.log('clickeddd')
 
     });
